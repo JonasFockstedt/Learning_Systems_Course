236fc2c933705f08673e7b410a008cf12ec15d27
Jonas <jonas.fo@live.se>
2020-03-06 15:58:46 +0100

    Now plots MSE for four different regression models.

--- Project/Task1.py
+++ Project/Task1.py
@@ -8,10 +8,13 @@ import matplotlib as mpl
 from sklearn.preprocessing import StandardScaler, normalize, Normalizer
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import explained_variance_score
+from sklearn.neural_network import MLPRegressor
+from random import randint
 
 mat = loadmat("Project Datasets/cnDieselTrain.mat")
-fig, ax = plt.subplots()
+fig = plt.figure()
 
 # Load data.
 Xtest = mat["cnTestX"].T
@@ -23,7 +26,8 @@ print(f'Shape of Xtest: {Xtest.shape}')
 print(f'Shape of Ytrain: {Ytrain.shape}')
 
 # Xval and Yval will be used as testing.
-Xtrain, Xval, Ytrain, Yval = train_test_split(Xtrain, Ytrain, test_size=0.2)
+Xtrain, Xval, Ytrain, Yval = train_test_split(
+    Xtrain, Ytrain, test_size=0.2, random_state=42)
 
 Xtrain_normalized = normalize(Xtrain)
 
@@ -34,36 +38,60 @@ Xtrain_reduced = pca.fit_transform(Xtrain)
 Xtest_reduced = pca.fit_transform(Xtest)
 #n = len(Xtrain_reduced)
 
-kf_10 = KFold(n_splits=10, shuffle=True, random_state=42)
-
 print(f'Shape of transformed Xtrain: {Xtrain_reduced.shape}')
 print(f'Shape of transformed Xtest: {Xtest_reduced.shape}')
 
 #print(np.cumsum(np.round(pca.explained_variance_ratio_, decimals=4)*100))
 
-regr = LinearRegression(n_jobs=-1)
-svr = SVR()
 
-lowest_mse = 0
-regression_models = [LinearRegression(n_jobs=-1), SVR()]
-mse = []
+def findOptimalNumberOfFeatures():
+    lowest_mse = 0
+    regression_models = [LinearRegression(
+        n_jobs=-1), SVR(), DecisionTreeRegressor(criterion='mse', splitter="best"), MLPRegressor()]
+    optimal_PCA_numbers = dict()
+    mse = []
+    standard_deviations = []
+    figures = []
 
-for number_of_components in range(1, 50):
-    pca = PCA(n_components=number_of_components)
-    Xtrain_reduced = pca.fit_transform(Xtrain)
-    score = -1*cross_val_score(regr, Xtrain_reduced, Ytrain.ravel(),
-                               cv=kf_10, scoring='neg_mean_squared_error', n_jobs=-1).mean()
-    mse.append(score)
-    if lowest_mse == 0:
-        lowest_mse = score
-    elif score < lowest_mse:
-        lowest_mse = score
-        optimal_components = number_of_components
+    for fig_number, model in enumerate(regression_models):
+        # Things to reset for every model.
+        kf_10 = KFold(n_splits=10, shuffle=True, random_state=randint(0, 100))
+        optimal_PCA_numbers.update({type(model).__name__: 0})
+        mse, standard_deviations = [], []
+        ax = fig.add_subplot(4, 1, fig_number+1)
+        optimal_components = 0
+        lowest_mse = 0
 
-print(
-    f'Number of components with least MSE: {optimal_components}\nLowest MSE: {lowest_mse}')
-ax.plot(np.arange(1, 50, 1), mse, '-o', color='blue')
-ax.set_ylabel('MSE')
-ax.set_xlabel('Number of PCAs')
-ax.set_title('MSE by number of PCAs')
-plt.show()
+        for number_of_components in range(1, 100):
+            # Update components for PCA.
+            pca = PCA(n_components=number_of_components)
+            Xtrain_reduced = pca.fit_transform(Xtrain)
+            score = -1*cross_val_score(model, Xtrain_reduced, Ytrain.ravel(),
+                                       cv=kf_10, scoring='neg_mean_squared_error', n_jobs=-1).mean()
+            mse.append(score)
+            standard_deviations.append(np.std(mse))
+            if lowest_mse == 0:
+                lowest_mse = score
+            elif score < lowest_mse:
+                lowest_mse = score
+                optimal_components = number_of_components
+                optimal_PCA_numbers.update(
+                    {type(model).__name__: optimal_components})
+
+        mse = np.array(mse)
+        standard_deviations = np.array(standard_deviations)
+        ax.plot(np.arange(1, 100, 1), mse, marker='.', color='blue',
+                label=f'Optimal PCAs: {optimal_components} at  MSE of {lowest_mse}.')
+        ax.fill_between(np.arange(1, 100, 1), mse + standard_deviations, mse-standard_deviations,
+                        alpha=0.5, facecolor='blue')
+        ax.set_ylabel('MSE')
+        ax.title.set_text(f'{type(model).__name__}')
+        ax.legend()
+
+    plt.xlabel('Number of PCAs')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    findOptimalNumberOfFeatures()
