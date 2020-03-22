359304ae69527f19521bcd806ec633e9bca28023
JonasFockstedt <jonas.fo@live.se>
2020-03-22 20:55:58 +0100

    Can now check for optimal number of features, calculate MSE of vanilla parameters, and do tuning of hyperparameters.

--- Project/Task3.py
+++ Project/Task3.py
@@ -5,8 +5,16 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import normalize, MinMaxScaler, StandardScaler
 from sklearn.svm import SVR
 from sklearn.model_selection import KFold, cross_val_score, train_test_split, GridSearchCV
+from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import SGDRegressor
+from sklearn.tree import DecisionTreeRegressor
 from random import randint
+import timeit
 
+
+start_time = timeit.default_timer()
 mat = loadmat("Project Datasets/PowerTrainData.mat")
 
 X_power_train = mat["powerTrainInput"].T
@@ -22,33 +30,129 @@ print(f'Shape of Ytrain: {Y_power_train.shape}')
 X_power_train_normalized = normalize(X_power_train)
 X_power_train_scaled = MinMaxScaler().fit_transform(X_power_train)
 
-pca = PCA(n_components=2)
-X_power_train_reduced = pca.fit_transform(X_power_train_scaled)
 
-models = {'SVR': SVR()}
+regression_models = {'SVR': SVR(), 'SGDRegressor': SGDRegressor(), 'DecisionTreeRegressor': DecisionTreeRegressor(), 'RandomForestRegressor': RandomForestRegressor(n_jobs=-1), 'KNeighborsRegressor' : KNeighborsRegressor(n_jobs=-1), 'MLPRegressor': MLPRegressor()}
+optimal_PCA_numbers = dict()
+mse_scores = {'SVR': [], 'SGDRegressor': [], 'DecisionTreeRegressor': [], 'RandomForestRegressor': [], 'KNeighborsRegressor': [], 'MLPRegressor': []}
+standard_deviation_scores = {'SVR': [], 'SGDRegressor': [], 'DecisionTreeRegressor': [], 'RandomForestRegressor': [], 'KNeighborsRegressor': [], 'MLPRegressor': []}
+grid_searches = dict()
+cv_scores = dict()
 
-def trainModels():
-    rndm_state = randint(1,100)
-    kf_10 = KFold(n_splits=10, shuffle=True, random_state=rndm_state)
-    for model in models.values():
-        score = cross_val_score(model, X_power_train_normalized, Y_power_train.ravel(), cv=kf_10, scoring='neg_mean_squared_error', n_jobs=1).mean()
-        print(score)
+
+# fig, ax = plt.subplots()
+
+# pca = PCA(n_components=2)
+# X_power_train_reduced = pca.fit_transform(X_power_train_scaled)
+# ax.scatter(X_power_train_scaled[:,0], X_power_train_scaled[:,1], c=Y_power_train.ravel(), edgecolor='black')
+# plt.title('Measurements of first two days')
+# plt.show()
+
+
+def findOptimalNumberOfFeatures():
+    print('***FINDING OPTIMAL NUMBER OF FEATURES FOR EACH MODEL***')
+    rndm_state = randint(0, 100)
+
+    for model in regression_models.keys():
+        print(f'Finding optimal number of features for {type(regression_models[model]).__name__} model...')
+        # Things to set up for every model.
+        kf_10 = KFold(n_splits=10, shuffle=True, random_state=rndm_state)
+        optimal_PCA_numbers.update({type(regression_models[model]).__name__: 0})
+        optimal_components = 0
+        lowest_mse = 0
+
+        # Begin with only 1 feature, then add one feature until all features have been tested.
+        for number_of_components in range(1, X_power_train.shape[1] + 1):
+            # Update components for PCA.
+            pca = PCA(n_components=number_of_components)
+            Xtrain_reduced = pca.fit_transform(X_power_train_normalized)
+            # Run 10-fold cross validation.
+            score = -1*cross_val_score(regression_models[model], Xtrain_reduced, Y_power_train.ravel(),
+                                       cv=kf_10, scoring='neg_mean_squared_error', n_jobs=-1).mean()
+
+            mse_scores[model].append(score)
+            standard_deviation_scores[model].append(np.std(mse_scores[model]))
+
+            # If first run, assign current score as lowest MSE.
+            if lowest_mse == 0:
+                lowest_mse = score
+                optimal_components = number_of_components
+                optimal_PCA_numbers.update(
+                    {type(regression_models[model]).__name__: optimal_components})
+            # If a new lowest MSE score is reached, save number of features.
+            elif score < lowest_mse:
+                lowest_mse = score
+                optimal_components = number_of_components
+                optimal_PCA_numbers.update(
+                    {type(regression_models[model]).__name__: optimal_components})
+
+def plotMSEScores():
+    fig  = plt.figure()
+    fig.subplots_adjust(hspace=.5)
+
+    for fig_number, model in enumerate(regression_models.keys(), 1):
+        ax = fig.add_subplot(3, 2, fig_number)
+        mse = np.array(mse_scores[model])
+        standard_deviations = np.array(standard_deviation_scores[model])
+        
+        # Plots the MSE.
+        ax.plot(np.arange(1, X_power_train.shape[1]+1, 1), mse_scores[model], marker='.', color='blue',
+                label=f'Optimal PCAs: {optimal_PCA_numbers[model]} at MSE of {min(mse)}.')
+        # Plots standard deviation with half opacity.
+        ax.fill_between(np.arange(1, X_power_train.shape[1] + 1, 1), mse + standard_deviations, mse - standard_deviations,
+                        alpha=0.5, facecolor='blue')
+        ax.set_ylabel('MSE')
+        ax.title.set_text(f'{type(regression_models[model]).__name__}')
+        ax.legend()
+
+    plt.xlabel('Number of PCAs')
+    plt.show()
+
+
+# def trainModels():
+#     rndm_state = randint(0,100)
+#     kf_10 = KFold(n_splits=10, shuffle=True, random_state=rndm_state)
+#     for model in regression_models.values():
+#         score = cross_val_score(model, X_power_train_normalized, Y_power_train.ravel(), cv=kf_10, scoring='neg_mean_squared_error', n_jobs=1).mean()
+#         print(f'MSE score before parameter tuning of {type(model).__name__} model: {score}')
 
 
 def parameterTuning():
-    parametersSVR = {'kernel': ('linear', 'rbf'), 'C': np.arange(1, 10, 1), 'epsilon': np.arange(0, 1, 0.1)}
-    regr = GridSearchCV(models['SVR'], parametersSVR, scoring='neg_root_mean_squared_error', n_jobs=-1, cv=10)
-    regr.fit(X_power_train_normalized, Y_power_train.ravel())
-    print(regr.best_estimator_)
-    print(regr.best_score_)
+    print('***TUNING PARAMETERS...***')
+    parametersSVR = {'kernel': ('linear', 'rbf'), 'degree': np.arange(1,5,1), 'gamma': ('scale', 'auto'), 'C': np.arange(1, 10, 1), 'epsilon': np.arange(0, 10, 0.1)}
+    parametersSGD = {'alpha': np.arange(0.00001, 0.005, 0.0005), 'max_iter': np.arange(1000, 5000, 500), 'epsilon': np.arange(0.01, 1, 0.05)}
+    parametersDT = {'criterion': ('mse', 'friedman_mse', 'mae'), 'splitter': ('best', 'random')}
+    parametersRF ={'n_estimators': np.arange(50, 200, 50)}
+    parametersKNN = {'n_neighbors': np.arange(1,8,1), 'weights': ('uniform', 'distance'), 'p': (1, 2)}
+    parametersMLP = {'solver': ('lbfgs', 'sgd', 'adam'), 'alpha': np.arange(0.00005, 0.001, 0.0005)}
 
-    
-fig, ax = plt.subplots()
+    parameters = {'SVR': parametersSVR, 'SGDRegressor': parametersSGD, 'DecisionTreeRegressor': parametersDT, 'RandomForestRegressor': parametersRF, 'KNeighborsRegressor': parametersKNN, 'MLPRegressor': parametersMLP}
+    for model in regression_models.keys():
+        print(f'Tuning the {model} model...')
+        # Perform grid search.
+        regr = GridSearchCV(regression_models[model], parameters[model], scoring='neg_root_mean_squared_error', n_jobs=-1, cv=10)
+        regr.fit(X_power_train_normalized, Y_power_train.ravel())
+        print(regr.best_estimator_)
+        print(regr.best_score_)
+        # Add best parameter combination to dictionary.
+        grid_searches.update({model: regr.best_estimator_})
+        # Add score of best parameters to dictionary
+        cv_scores.update({model: regr.best_score_})
 
-ax.scatter(X_power_train_scaled[:,0], X_power_train_scaled[:,1], edgecolor='black')
-plt.title('Measurements of first two days')
-plt.show()
+def plotCVScores():
+    plt.ylabel('Mean squared error regression loss (higher is better)')
+    plt.title('Cross-validation scores of regression models')
+    bars = plt.bar(list(cv_scores.keys()), list(cv_scores.values()), align='center')
+    # Add value above every bar.
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x(), yval + 0.05, yval)
+    plt.show()
+
 
 if __name__ == '__main__':
-    trainModels()
+    #trainModels()
+    findOptimalNumberOfFeatures()
+    #plotMSEScores()
     parameterTuning()
+    plotCVScores()
+    print(f'Runtime: {timeit.default_timer() - start_time}s')
